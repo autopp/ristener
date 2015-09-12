2646b1508437923f14ffba67c604621730d1c171
autopp <autopp.inc@gmail.com>
2015-09-13 00:12:01 +0900

    add default port number

--- lib/ristener.rb
+++ lib/ristener.rb
@@ -5,7 +5,9 @@ require 'ristener/version'
 # @author autopp <autopp.inc@gmail.com>
 #
 module Ristener
+  DEFAULT_PORT = 5678
 end
 
+require 'ristener/ristener_error'
 require 'ristener/server'
 require 'ristener/client'
