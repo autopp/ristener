cbd480a1eeadc6ac5ebac268e4da7b295ba551ca
autopp <autopp.inc@gmail.com>
2015-09-20 16:13:55 +0900

    fix typo and add command description

--- lib/ristener/cli.rb
+++ lib/ristener/cli.rb
@@ -7,12 +7,14 @@ module Ristener
   #
   class CLI < Thor
     option :ristenfile, aliases: :f, type: :string, default: './RistenFile'
+    desc 'server', 'Run as server mode'
     def server
-      Server.run(ristenfile)
+      Server.run(options[:ristenfile])
     end
 
     option :host, aliases: :H, type: :string, default: 'localhost'
     option :port, aliases: :p, type: :numeric, default: DEFAULT_PORT
+    desc 'client COMMAND', 'Run as client mode'
     def client(command)
       Client.new.run(command, options[:host], options[:port])
     end
